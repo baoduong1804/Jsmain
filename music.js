382ba84bc3285f7a889c5e35f6695068040bdc56
baoduong <baoduong.18042k3@gmail.com>
2022-10-12 20:19:49 +0700

    12/10

--- music.js
+++ music.js
@@ -1,54 +1,124 @@
 
-
 const song = document.getElementById("song");
 const playBtn =document.querySelector('.player-inner');
 const nextBtn =document.querySelector('.play-forward');
 const prevBtn =document.querySelector('.play-back');
-const musics =['saveMe.mp3','holo.mp3','spark.mp3','summer.mp3'];
+const durationTime = document.querySelector('.duration');
+const remainingTime = document.querySelector('.remaining');
+const rangeBar = document.querySelector('.range');
+const musicName = document.querySelector('.music-name');
+const musicImage = document.querySelector('.music-thumb img');
+const changeBg = document.querySelector('.change-bg-img');
+const changeBgWhite = document.querySelector('.change-bg-white');
+const body = document.querySelector('body');
+//Danh sách phát
+const musics =[
+    {
+        id:1,
+        title:'Save Me',
+        file:'saveMe.mp3',
+        image:'https://wikikienthuc.com/wp-content/uploads/2019/11/dich-loi-bai-save-me-deamn.jpg'
+    },
+    {
+        id:2,
+        title:'Holo',
+        file:'holo.mp3',
+        image:'https://d34ojwe46rt1wp.cloudfront.net/wp-content/uploads/2020/04/san-holo.jpg'
+    },
+    {
+        id:3,
+        title:'Spark',
+        file:'spark.mp3',
+        image:'https://images.squarespace-cdn.com/content/v1/5b33cce0cef3725c510dd56f/1613100826972-R35HWBYS3QSLCLRDSOH9/Spark+Logo_Door_03.jpg'
+    },
+    {
+        id:2,
+        title:'Summer',
+        file:'summer.mp3',
+        image:'https://i.ytimg.com/vi/4loUFomAgck/maxresdefault.jpg'
+    }
+]
 let isPlaying =true;
 var indexMusic = 0;
+//Hoán đổi isPlaying
+function exchange(){
+    if(isPlaying){
+        isPlaying=false;
+    }else{
+        isPlaying=true;
+    }
+}
+//Xử lý khi ấn nút Play
 playBtn.addEventListener('click',playPause);
 function playPause(){
     if(isPlaying){
-        song.play();
+        song.play();//nhạc chạy
         isPlaying=false;
         playBtn.innerHTML = `<ion-icon name="pause"></ion-icon>`;
+        musicImage.parentElement.classList.add('isplaying');
     }else{
-        song.pause();
+        song.pause();//nhạc dừng
         isPlaying=true;
         playBtn.innerHTML = `<ion-icon name="play" class="play-icon"></ion-icon>`;
+        musicImage.parentElement.classList.remove('isplaying');
     }
 }
+//Xử lý khi ấn nút Play Forward
 nextBtn.onclick = function(){
     ++indexMusic;
     if(indexMusic == musics.length){
         indexMusic = 0;
     }
-    song.setAttribute('src',`music/${musics[indexMusic]}`);
-    console.log(indexMusic);
-    if(isPlaying){
-        isPlaying=false;
-    }else{
-        isPlaying=true;
-    }
-    playPause();
+    init(indexMusic);
 }
+//Xử lý khi ấn nút Play Back
 prevBtn.onclick = function(){
     --indexMusic;
     if(indexMusic==-1){
         indexMusic = musics.length-1;
-    }
-    
-    song.setAttribute('src',`music/${musics[indexMusic]}`);
-    console.log(indexMusic);
-    if(isPlaying){
-        isPlaying=false;
-    }else{
-        isPlaying=true;
-    }
-    playPause()
+    };
+    init(indexMusic);
 }
-
-
-
-
+//Qua bài mới khi kết thúc
+song.onended = function(){
+    ++indexMusic;
+    init(indexMusic);
+}
+//Xử lý thời gian nhạc
+function displayTimer(){
+const {duration ,currentTime} = song;
+//duration là tổng thời lượng nhạc
+//currentTime là thời lượng bài hát đang chạy
+    rangeBar.max = duration;
+    rangeBar.value = currentTime;
+    remainingTime.textContent = formatTimer(currentTime)
+    durationTime.textContent = formatTimer(Math.floor(duration)-Math.floor(currentTime));
+}
+setInterval(displayTimer,300);
+//Xử lý thời lượng nhạc thành phút:giây
+function formatTimer(number){
+    const minutes = Math.floor(number/60);
+    const seconds = Math.floor(number - minutes*60);
+    return `${minutes < 10 ? '0'+minutes:minutes}:${seconds < 10 ? '0'+seconds:seconds}`
+}
+//Tua nhạc
+rangeBar.oninput = function(){
+        song.currentTime = rangeBar.value;
+}
+//Thay đổi nhạc,ảnh và tên
+function init(indexMusic){
+    displayTimer();
+    song.setAttribute('src',`music/${musics[indexMusic].file}`);
+    musicImage.setAttribute('src',musics[indexMusic].image);
+    musicName.textContent = musics[indexMusic].title;
+    exchange();
+    playPause();
+    
+}
+//Thay đổi background
+changeBg.onclick = function(){
+    body.setAttribute('background',musics[indexMusic].image);
+}
+changeBgWhite.onclick = function(){
+    body.removeAttribute('background');
+}
