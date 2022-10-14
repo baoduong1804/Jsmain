826d444d05a3d467ee3111f433243da953d81ca8
baoduong <baoduong.18042k3@gmail.com>
2022-10-15 02:04:00 +0700

    2:03 15/10

--- music.js
+++ music.js
@@ -1,8 +1,8 @@
 
 const song = document.getElementById("song");
-const playBtn =document.querySelector('.player-inner');
-const nextBtn =document.querySelector('.play-forward');
-const prevBtn =document.querySelector('.play-back');
+const playBtn = document.querySelector('.player-inner');
+const nextBtn = document.querySelector('.play-forward');
+const prevBtn = document.querySelector('.play-back');
 const durationTime = document.querySelector('.duration');
 const remainingTime = document.querySelector('.remaining');
 const rangeBar = document.querySelector('.range');
@@ -11,31 +11,37 @@ const musicImage = document.querySelector('.music-thumb img');
 const changeBg = document.querySelector('.change-bg-img');
 const changeBgWhite = document.querySelector('.change-bg-white');
 const body = document.querySelector('body');
-//Danh sách phát
+const infiniteBtn = document.querySelector('.play-infinite');
+const repeatBtn = document.querySelector('.play-repeat');
+let isPlaying =true;
+let isRepeat = false;
+let isInfinite = false;
+var indexMusic = 0;
+//List bài hát
 const musics =[
     {
         id:1,
-        title:'Holo',
-        file:'holo.mp3',
-        image:'https://d34ojwe46rt1wp.cloudfront.net/wp-content/uploads/2020/04/san-holo.jpg'
+        title:'Đến để đi',
+        file:'De-Den-De-Di.mp3',
+        image:'https://img.theinfluencer.vn/thumb_w/850/uploads/2021/10/li4JR5EBvckEWNhAXvkte85DXPUQRqvwZddhvAS1.png'
     },
     {
         id:2,
-        title:'Summer',
-        file:'summer.mp3',
-        image:'https://i.ytimg.com/vi/4loUFomAgck/maxresdefault.jpg'
-    },
-    {
-        id:3,
         title:'Tình yêu ngủ quên',
         file:'Tinh-Yeu-Ngu-Quen.mp3',
         image:'https://i.ytimg.com/vi/eukA1NGSM5w/maxresdefault.jpg'
     },
     {
-        id:4,
+        id:3,
         title:'Gác lại Âu lo',
         file:'Gac-Lai-Au-Lo.mp3',
         image:'https://photo-cms-tpo.zadn.vn/600x315/Uploaded/2022/qqc_wqdun/2020_07_23/da_lab_miu_le_CQBP.png'
+    },
+    {
+        id:4,
+        title:'Holo',
+        file:'holo.mp3',
+        image:'https://d34ojwe46rt1wp.cloudfront.net/wp-content/uploads/2020/04/san-holo.jpg'
     },
     {
         id:5,
@@ -54,10 +60,27 @@ const musics =[
         title:'Bỏ em vào Balo',
         file:'Bo-Em-Vao-Balo.mp3',
         image:'https://i.ytimg.com/vi/FW5aNm9cx9A/maxresdefault.jpg'
+    },
+    {
+        id:8,
+        title:'Nụ cười em là nắng',
+        file:'Nu-Cuoi-Em-La-Nang.mp3',
+        image:'https://i.ytimg.com/vi/EsVgzTJf4TA/maxresdefault.jpg'
+    },
+    {
+        id:9,
+        title:'Ngày đầu tiên',
+        file:'Ngay-Dau-Tien.mp3',
+        image:'https://i.ytimg.com/vi/rIXhXaQ8tiM/maxresdefault.jpg'
+    },
+    {
+        id:10,
+        title:'Lời anh chưa thể nói',
+        file:'Loi-Anh-Chua-The-Noi.mp3',
+        image:'https://i.ytimg.com/vi/yfpiynLlcDA/maxresdefault.jpg'
+        // image:'https://i.ytimg.com/vi/IagHzELYX6U/maxresdefault.jpg'
     }
 ]
-let isPlaying =true;
-var indexMusic = 0;
 //Hoán đổi isPlaying
 function exchange(){
     if(isPlaying){
@@ -65,6 +88,26 @@ function exchange(){
     }else{
         isPlaying=true;
     }
+}
+//Hoán đổi isRepeat,thay đổi màu khi ấn play-repeat
+repeatBtn.onclick =function(){
+    if(isRepeat){
+     repeatBtn.removeAttribute('style');
+     isRepeat =false;
+    } else {
+     isRepeat =true;
+     repeatBtn.style.color = '#6103f7ab';
+    }
+}
+//Hoán đổi isInfinite,thay đổi màu khi ấn play-infinite
+infiniteBtn.onclick =function(){
+   if(isInfinite){
+    infiniteBtn.removeAttribute('style');
+    isInfinite =false;
+   } else {
+    isInfinite =true;
+    infiniteBtn.style.color = '#008b85';
+   }
 }
 //Xử lý khi ấn nút Play
 playBtn.addEventListener('click',playPause);
@@ -99,11 +142,23 @@ prevBtn.onclick = function(){
 }
 //Qua bài mới khi kết thúc
 song.onended = function(){
-    ++indexMusic;
-    if(indexMusic==musics.length){
-        indexMusic=0;
+    //Xử lý repeat 1 bài 1 lần
+    if(!isRepeat){
+        ++indexMusic;
+    } else {
+        indexMusic;
+        isRepeat= false;
+        repeatBtn.removeAttribute('style');
     }
-    init(indexMusic);
+    // Xử lí lặp lại vô hạn music list
+    if(isInfinite && indexMusic==musics.length){    
+            indexMusic=0;
+    } else if(!isInfinite && indexMusic==musics.length) {
+        --indexMusic;
+        playPause()
+    }
+    console.log(indexMusic)
+    init(indexMusic);       
 }
 //Xử lý thời gian nhạc
 function displayTimer(){
@@ -134,11 +189,18 @@ function init(indexMusic){
     musicName.textContent = musics[indexMusic].title;
     exchange();
     playPause();
+    console.log(indexMusic)
 }
 //Thay đổi background
 changeBg.onclick = function(){
     body.setAttribute('background',musics[indexMusic].image);
+    changeBgWhite.removeAttribute('style');
+    changeBg.style.boxShadow = '0 0 0'
 }
 changeBgWhite.onclick = function(){
     body.removeAttribute('background');
+    changeBg.removeAttribute('style');
+    changeBgWhite.style.boxShadow = '0 0 0'
 }
+changeBgWhite.style.boxShadow = '0 0 0'
+init(indexMusic)
